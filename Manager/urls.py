40f6084bc1bf948ed28f3decd231376e42f3da2c
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2020-03-22 15:47:15 -0700

    update add employee url

--- Manager/urls.py
+++ Manager/urls.py
@@ -16,11 +16,12 @@ urlpatterns:  pa    2. Add a URL to th('blog/', include('blog.urls'))
 
 from django.contrib import admin
 from django.urls import path
-
 from django.urls import include
 from .views import home_view
+from . import views
 
 urlpatterns = [
     path('homepage', home_view, name = 'home'),
-    path('', home_view, name = 'home')
+    path('', home_view, name = 'home',
+    path('employee',views.employee, name='employee'),
 ]
