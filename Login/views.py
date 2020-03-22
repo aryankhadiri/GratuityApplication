3853c9fb8a154c2679adad8f19807572084b8f2e
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2020-03-21 18:46:28 -0700

    cleaned up

--- Login/views.py
+++ Login/views.py
@@ -1,38 +1,46 @@
+"""
+This file contains a function-based view for the Login page.
+"""
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+#       IMPORTS                 
+# -----------------------------------------------------------------------------------------------------------------------------------------
 from django.shortcuts import render, redirect
 from .forms import LoginForm
 from django.http import HttpResponse
 from .models import User
 from django.contrib.auth import authenticate, login
 
-
-# Create your views here.
+# -----------------------------------------------------------------------------------------------------------------------------------------
+#       LOGIN             
+# -----------------------------------------------------------------------------------------------------------------------------------------
 def login_view(request):
+
     error = ''
     form = LoginForm()
+
     if request.user.is_authenticated:
-        
-        # redirect the user based on their management status to their homepage
+        # redirect the user (based on their management status) to their appropriate homepage
         if request.user.manager == True:
             return redirect('/manager/')
+
     if request.method == 'POST':
-        if form.is_valid:
-            
-            useremail = request.POST.get('email')
-            userpassword = request.POST.get('password')
-            user = authenticate(request, email = useremail, password = userpassword)
+        if form.is_valid():
+            user_email = form.cleaned_data['email']
+            user_password = form.cleaned_data['password']
+            user = authenticate(request, email = user_email, password = user_password)
 
             if user is not None:
                 login(request, user)
                 return redirect('/manager/')
             else:
-                error = 'Incorrect Email/Password!'
-                
+                error = 'Incorrect email/password combination.'
         
-  
     context = {
         'form':form,
         'error':error
     }
     
     return render(request,'login.html', context)
+# -----------------------------------------------------------------------------------------------------------------------------------------
     
