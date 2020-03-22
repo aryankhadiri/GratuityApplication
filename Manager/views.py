1239cfaef8ce388fc91142d66d02ab3fb6294781
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2020-03-22 15:45:28 -0700

    Update views.py

--- Manager/views.py
+++ Manager/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from django.contrib import messages
+
 # Create your views here.
 def home_view(request):
     Hello = 'Hello Boys..Welcome!'
