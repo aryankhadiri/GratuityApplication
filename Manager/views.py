96f794317b2c3cbbd71cf7e0329a7d8988471531
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2020-03-22 15:44:36 -0700

    employee view

--- Manager/views.py
+++ Manager/views.py
@@ -7,3 +7,19 @@ def home_view(request):
         'Hello':Hello
     }
     return render(request, 'home.html', context)
+
+def employee(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        point = request.POST['point']
+        phone = request.POST['phone']
+        if Employee.objects.filter(name=name).exists():
+            messages.error(request,'This Employess is already added')
+            return redirect ('employee')
+        else:  
+            employee = Employee.objects.create(name=name, point=point, phone=phone)
+            employee.save()
+       #    print (employee)
+            messages.success(request,'Employee succesfully added')
+            return redirect ('employee')
+    return render (request, 'pages/_index.html')
