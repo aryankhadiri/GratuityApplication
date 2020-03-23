881eefeaf3a7a16357b50abedc195565ace5c5dc
Aryan Khadiri <aryankhadiri@gmail.com>
2020-03-22 20:21:00 -0700

    updated the database with floatfield for point and charfield for phonenumber

--- Manager/models.py
+++ Manager/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Employee (models.Model):
     name = models.CharField (max_length = 100)
-    point = models.IntegerField()
-    phone = models.IntegerField()
+    point = models.FloatField()
+    phone = models.CharField(max_length = 10)
 
     def __str__(self):
         return self.name
