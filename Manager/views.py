7327257320a4cf2b961f947aae4a8b7ba0dc7d52
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2020-03-23 17:28:09 -0700

    Update list view

--- Manager/views.py
+++ Manager/views.py
@@ -16,9 +16,28 @@ def home_view(request):
 #@login_required
 #login_required is functional, just commented out for development process
 def employee(request):
-    form = EmployeeForm(request.POST or None)
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return redirect('employee')
-    return render(request, 'pages/_index.html', {'form': form})
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid(): 
+            name = request.POST['name']
+            if Employee.objects.filter(name=name).exists():
+                messages.error(request, 'Employee was already Added')
+                return redirect('employee')
+            else:   
+                form.save()
+                messages.success(request, 'Employee Successfully Added')
+                return redirect('employee')
+        else:
+            messages.error(request, 'The form is invalid.')
+
+        return render(request, 'pages/_index.html', {'form': form})
+    else:
+        form = EmployeeForm()
+        return render(request, 'pages/_index.html', {'form': form})
+    
+ def listEmployee(request):
+    queryset = Employee.objects.all()
+    context = {
+        "list":queryset
+    }
+    return render (request,'pages/list.html', context)
