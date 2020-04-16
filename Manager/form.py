87147c7273463409a04f7930982d9a26b9f1a1c8
Aryan Khadiri <aryankhadiri@gmail.com>
2020-04-16 02:46:04 -0700

    working on employee design

--- Manager/form.py
+++ Manager/form.py
@@ -5,9 +5,18 @@ from django import forms
 
 
 class EmployeeForm(forms.ModelForm):
-    name = forms.CharField(max_length = 100)
-    point = forms.FloatField()
-    phone = forms.CharField(max_length = 10)
+    name = forms.CharField(max_length = 100, widget = forms.TextInput(attrs={
+        'Class':'nameField',
+        'id':'nameField',
+        'placeholder': 'David Watson'}))
+    point = forms.FloatField(widget = forms.TextInput(attrs={
+        'Class':'pointField',
+        'id':'pointField',
+        'placeholder': '0.2'}))
+    phone = forms.CharField(max_length = 10, widget = forms.TextInput(attrs={
+        'Class':'phoneField',
+        'id':'phoneField',
+        'placeholder': '8887776655'}))
     class Meta:
         model = Employee
         fields = "__all__"
