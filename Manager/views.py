6e27c86a0af8e9d98b1484ad1f3daf083639bdd2
Aryan Khadiri <aryankhadiri@gmail.com>
2020-04-21 23:00:03 -0700

    search option added to the manager/home-functional

--- Manager/views.py
+++ Manager/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404,redirect
 from django.http import HttpResponse
-from .models import *
+from .models import Employee
+from Employees.models import Tip, Form
 from django.contrib import messages
 from .form import EmployeeForm
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from datetime import timedelta
 
 # Create your views here.
 #@login_required
@@ -13,9 +16,19 @@ def home_view(request):
         return redirect('/employee/')
     title = 'Dashboard | Home'
 
-    Hello = 'Hello Boys..Welcome!'
+    if request.method == 'POST':
+        start_date = request.POST['start_date_input']
+        end_date = request.POST['end_date_input']
+        forms = Form.objects.filter(date__gte=start_date).filter(date__lte=end_date)
+        return render(request, 'home.html', {'allForms':forms})
+    
+    today_date = datetime.today().date()
+    week_ago = today_date - timedelta(days=7)
+    allForms = Form.objects.filter(date__gte = week_ago)
+    #fetch all the forms submitted between week ago and today. 
     context = {
-        'title': title
+        'title': title,
+        'allForms':allForms
     }
     return render(request, 'home.html', context)
 #@login_required
@@ -25,6 +38,7 @@ def add_employee_view(request):
         return redirect('/employee/')
     title = 'Adding Employee'
     if request.method == 'POST':
+        
         form = EmployeeForm(request.POST)
         if request.POST.get('cancel_button') == '':
             return redirect('home')
